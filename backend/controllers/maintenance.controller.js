9985ca01104c865027b57ef7b3b7c7a013ce139a
Ibrahim-Lmlilas <lmlilas.ibrahim@student.youcode.ma>
2025-12-15 02:23:07 +0100

    maintenance

--- backend/controllers/maintenance.controller.js
+++ backend/controllers/maintenance.controller.js
@@ -306,7 +306,6 @@ module.exports = {
     getMaintenanceById,
     getMaintenancesByVehicule,
     planifierMaintenance,
-    calculerEcheances,
     marquerCommeEffectuee,
     updateMaintenance,
     deleteMaintenance,
